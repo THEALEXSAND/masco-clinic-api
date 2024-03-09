6a59a958c9900f46e9cef2c905f8e8787d209e3c
THEALEXSAND <gohangaymer888@outlook.es>
2024-03-08 23:12:04 -0400

    CustomerSeeder logic improved in order to add MedicalHistory

--- database/seeders/MedicalHistorySeeder.php
+++ database/seeders/MedicalHistorySeeder.php
@@ -2,15 +2,16 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Pet;
 
-class PetSeeder extends Seeder
+class MedicalHistorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        //
     }
 }
