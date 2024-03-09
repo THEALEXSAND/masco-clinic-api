3ce5d24015d776e49703884f699d5bd7bccdd444
THEALEXSAND <gohangaymer888@outlook.es>
2024-03-08 23:15:12 -0400

    MedicalHistoryResorces added

---
+++ app/Http/Resources/MedicalHistoryResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MedicalHistoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'pet_id' => $this->pet_id,
+            'obervacion' => $this->observacion,
+        ];
+    }
+}
