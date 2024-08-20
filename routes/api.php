85efd672d1601f4aacb69a37b90417c81ae26333
THEALEXSAND <gohangaymer888@outlook.es>
2024-08-19 21:36:30 -0400

    [FEATURE] Breeds, Customer, Pets, Species and User API routes with a fallback ADDED

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\BreedController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\SpecieController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v2', 'namespace' => 'App\Http\Controllers'], function () {
-    Route::apiResource('customer', CustomerController::class);
+    Route::apiResources([
+        'breeds' => BreedController::class,
+        'customers' => CustomerController::class,
+        'pets' => PetController::class,
+        'species' => SpecieController::class,
+        'users' => UserController::class
+    ]);
+
+    Route::fallback(function () {
+        return response([
+            'message' => 'Error 404. The route does not exist. Not Found!',
+            'status' => 404
+        ], 404);
+    });
 });
